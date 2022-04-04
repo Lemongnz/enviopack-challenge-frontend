45b2d11ba93f67db7d0a1b6368cb7b877509339a
Lemongnz <lemongnz@gmail.com>
2022-04-04 10:57:08 -0300

    ver carrito

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -2,12 +2,20 @@ import { Container, ImgContainer, InfoContainer, AddToCart } from "./styles";
 import prodimg from "../../../public/image-product.jpg";
 import { useDispatch } from "react-redux";
 import { addProduct } from "../../../reducers/product";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function ProductCard(product) {
+  const [productAdded, setProductAdded] = useState(false);
   const dispatch = useDispatch();
+  const navigateTo = useNavigate();
 
   const addProductToCart = () => {
+    if (productAdded) {
+      return navigateTo("/cart");
+    }
     dispatch(addProduct(product));
+    setProductAdded(true);
   };
 
   return (
@@ -20,7 +28,9 @@ export default function ProductCard(product) {
           <p>{product.title}</p>
           <span>${product.price}</span>
         </InfoContainer>
-        <AddToCart onClick={addProductToCart}>Agregar al carrito</AddToCart>
+        <AddToCart onClick={addProductToCart}>
+          {productAdded ? "Ver Carrito" : "Agregar al carrito"}
+        </AddToCart>
       </Container>
     </>
   );
